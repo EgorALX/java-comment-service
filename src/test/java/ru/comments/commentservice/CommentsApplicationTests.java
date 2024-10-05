528870cf98bc8ee87299b58409a874fcd3b07291
Egor <alexanoffegor@yandex.ru>
2024-10-05 16:14:00 +0300

    init: start app

---
+++ src/test/java/ru/comments/commentservice/CommentsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.comments.commentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
